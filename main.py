3f9618421874c8deeab7a85bb7fac629f83efb71
Prabhakar <prabhakarkumar313@gmail.com>
2024-06-16 17:32:49 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+#
+# l = []
+#
+# for i in os.listdir("C:\\prabhakar\\DETAILED CONTENT\\machine learning\\projects\\6. Insurance Fraud Detection\\insuranceFraudDetection\\code\\fraudDetection"):
+#     l.append(i)
+#
+# print(l)
+print("helo")
